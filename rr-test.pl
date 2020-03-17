505a7dab9626373b155e50ca1a38973c7f8c269b
Vladi Belperchinov-Shabanski <cade@bis.bg>
2020-03-17 19:24:13 +0200

    maint

--- rr-test.pl
+++ rr-test.pl
@@ -9,11 +9,19 @@
 ##
 ##############################################################################
 use strict;
+use Data::Tools::Time;
 
+=pod
 
+  in 2days 8hrs 11min 23sec
+  next tue at 12:00
+  next apr 1st
+  next last tue of mar
 
+  repeat every year|month|day
+  repeat every 6hrs
 
-
+=cut
 
 
 while(4)
@@ -33,83 +41,76 @@ sub parse_time
 {
   my $ta = [ @_ ];
   
-  my $ti = { TS => time(), TR => 0 }; # time info, time stamp and time repeat
+  my $ts = time();
+  my $tr = 0;
   
   while( @$ta )
     {
     $_ = shift @$ta;
     if( /^in/ )
       {
-      parse_time_in( \@ti, $ti );
+      $ts = time() + parse_time_in( $ta );
       next;
       }
     elsif( /^repeat/ )
       {
-      parse_time_repeat( \@ti, $ti );
+      $tr = parse_time_in( $ta );
       next;
       }
     else
       {
-      die "invalid timespec at [@$ta]\n";
+      die "invalid timespec at [$_]\n";
       }  
     }
-
+  return $ts;
 }
 
 sub parse_time_in
 {
   my $ta = shift;
-  my $ti = shift;
+  
+  my $tt;
 
   while( @$ta )
     {
     $_ = shift @$ta;
     if( /^repeat/ )
       {
-      return parse_time_repeat( \@ti, $ti );
       }
-    elsif( /(\d+)d(ays?)?/ )         # days day d
+    elsif( /^(\d+)d(ays?)?$/ )         # days day d
       {
-      $ti->{ 'TS' } += $1 * 24 * 60 * 60;
+      $tt += $1 * 24 * 60 * 60;
       next;
       }
-    elsif( /(\d+)(h(ours?)?|hrs?)/ ) # hours hour hrs hr h 
+      # TODO: months, years, weeks
+#    elsif( /^(\d+)(h(ours?)?|hrs?)$/ ) # hours hour hrs hr h 
+    elsif( /^(\d+)(h(ours?|rs?)?|d(ays?)?|w(eeks?|wks?)?|mo(n|nths?)?|y(ears?|rs?)?)$/ ) # hours hour hrs hr h 
       {
-      $ti->{ 'TS' } += $1 * 60 * 60;
+      my $type = uc substr( $2, 0, 1 );
+      $tt +=          $1 * 60 * 60 if $type eq 'H'; # hours
+      $tt +=     $1 * 24 * 60 * 60 if $type eq 'D'; # days
+      $tt += $1 * 7 * 24 * 60 * 60 if $type eq 'W'; # weeks
+
+      $tt = utime_add_ymd( $tt, 0, $1, 0 ) if $type eq 'M'; # months
+      $tt = utime_add_ymd( $tt, $1, 0, 0 ) if $type eq 'Y'; # years
       next;
       }
-    elsif( /(\d+)(m(in(utes?)?)?)/ ) # minutes minute min m
+    elsif( /^(\d+)(m(in(utes?)?)?)$/ ) # minutes minute min m
       {
-      $ti->{ 'TS' } += $1 * 60;
+      $tt += $1 * 60;
       next;
       }
-    elsif( /(\d+)(s(ec(onds?)?)?|secs)/ ) # seconds second secs sec s
+    elsif( /^(\d+)(s(ec(ond)?s?)?)$/ ) # seconds second secs sec s
       {
-      $ti->{ 'TS' } += $1;
+      $tt += $1;
       next;
       }
     else
       {
-      die "invalid timespec at [@$ta]\n";
+      unshift @$ta, $_;
+      return $tt;
       }  
     }
-}
-
-sub parse_time_repeat
-{
-  my $ta = shift;
-  my $ti = shift;
   
+  return $tt;
 }
-
-=pod
-
-  in 2days 8hrs 11min 23sec
-  next tue at 12:00
-  next apr 1st
-  next last tue of mar
-
-  repeat every year|month|day
-  repeat every 6hrs
-
-=cut
